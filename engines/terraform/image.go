3f8d54bebdc69707caee75c0e052acd9f90d7ae2
Tim Holm <timothy.j.holm@gmail.com>
2025-10-02 13:55:01 +1000

    chore(cli): remove tokens from orginal command compacting

--- engines/terraform/image.go
+++ engines/terraform/image.go
@@ -1,3 +1,3 @@
 package terraform
 
-var imageModuleRef = "github.com/nitrictech/suga//engines/terraform/modules/image?depth=1&ref=main"
+var imageModuleRef = "github.com/nitrictech/suga//engines/terraform/modules/image?depth=1&ref=3b953b8af51d1c874c9038ee9dae56bced1bce5a"
