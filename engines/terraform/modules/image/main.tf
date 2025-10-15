e75fff76c39088e091a4706b921d46a4b15a7bea
Tim Holm <tim.holm@nitric.io>
2025-10-15 16:48:25 +1100

    fix(engines): always re-trigger builds for now (#124)

--- engines/terraform/modules/image/main.tf
+++ engines/terraform/modules/image/main.tf
@@ -2,7 +2,7 @@ locals {
   context_path     = abspath("${path.root}/../../../${var.build_context != "." ? var.build_context : ""}")
   original_command = join(" ", compact([data.external.inspect_base_image.result.entrypoint, data.external.inspect_base_image.result.cmd]))
   image_id         = var.image_id == null ? docker_image.base_service.image_id : var.image_id
-  build_trigger    = var.image_id == null ? sha1(join("", [for f in fileset(local.context_path, "**") : filesha1("${local.context_path}/${f}")])) : var.image_id
+  build_trigger    = timestamp()
 }
 
 resource "docker_image" "base_service" {
