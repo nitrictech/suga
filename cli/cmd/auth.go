642e18d1d99c0c8cc601ac880047fe8a8c4c3649
Rak Siva <rak.siva@nitric.io>
2025-10-28 09:23:30 -0600

    fix: prevents device authorization flow when Personal Access Token set

--- cli/cmd/auth.go
+++ cli/cmd/auth.go
@@ -2,7 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/nitrictech/suga/cli/internal/style"
+	"github.com/nitrictech/suga/cli/internal/style/icons"
 	"github.com/nitrictech/suga/cli/internal/version"
 	"github.com/nitrictech/suga/cli/pkg/app"
 	"github.com/samber/do/v2"
@@ -16,6 +19,12 @@ func NewLoginCmd(injector do.Injector) *cobra.Command {
 		Short: fmt.Sprintf("Login to %s", version.ProductName),
 		Long:  fmt.Sprintf("Login to the %s CLI.", version.ProductName),
 		Run: func(cmd *cobra.Command, args []string) {
+			if os.Getenv(AccessTokenEnvVar) != "" {
+				fmt.Printf("\n%s Already authenticated using a Personal Access Token.\n", style.Yellow(icons.Info))
+				fmt.Printf("  To use device authorization flow, unset the %s environment variable.\n", AccessTokenEnvVar)
+				return
+			}
+
 			app, err := do.Invoke[*app.AuthApp](injector)
 			if err != nil {
 				cobra.CheckErr(err)
@@ -32,6 +41,12 @@ func NewLogoutCmd(injector do.Injector) *cobra.Command {
 		Short: fmt.Sprintf("Logout from %s", version.ProductName),
 		Long:  fmt.Sprintf("Logout from the %s CLI.", version.ProductName),
 		Run: func(cmd *cobra.Command, args []string) {
+			if os.Getenv(AccessTokenEnvVar) != "" {
+				fmt.Printf("\n%s Cannot logout when authenticated using a Personal Access Token.\n", style.Yellow(icons.Info))
+				fmt.Printf("  To stop using the token, unset the %s environment variable.\n", AccessTokenEnvVar)
+				return
+			}
+
 			app, err := do.Invoke[*app.AuthApp](injector)
 			if err != nil {
 				cobra.CheckErr(err)
